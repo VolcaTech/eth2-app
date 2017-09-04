950ea673c53a29a523cc757f2a49295827aff133
Mikhail <m.dobrokhvalov@gmail.com>
2017-09-04 17:09:54 +0300

    extrafiles deleted

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-var DirectProxy = artifacts.require("./DirectProxy.sol");
 var VerifiedProxy = artifacts.require("./VerifiedProxy.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(DirectProxy);
     // "0x1b019c6f52c39e07e6c396ee1d0f957d3832d95d" - tesnet
     // 0xf695e673d7d159cbfc119b53d8928ceca4efe99e  - ropsten
-    var verifier = "0xf695e673d7d159cbfc119b53d8928ceca4efe99e";  // "0x1b019c6f52c39e07e6c396ee1d0f957d3832d95d";//web3.eth.accounts[1];//"0xeaa6ca321825a54665286a981d069bb53aa62e7f";
-    deployer.deploy(VerifiedProxy, verifier, web3.toWei(0.01,  "ether"));
+    // HARDCODED VALUES for proof-of-concept 
+    var verifier = "0xf695e673d7d159cbfc119b53d8928ceca4efe99e";
+    var fee = 0.01;
+    deployer.deploy(VerifiedProxy, verifier, web3.toWei(fee,  "ether"));
 };
