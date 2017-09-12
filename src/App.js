0d55a1030d11d9a4d05596885573a008c504bc1b
sashamihaha <sashamihaha@gmail.com>
2017-09-13 02:54:31 +0300

    link-footer bottom

--- src/App.js
+++ src/App.js
@@ -8,73 +8,76 @@ import LinkFooter from './components/common/LinkFooter';
 
 class App extends Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {
-      	  web3Loaded: false,
-      	  noWeb3: false
-      };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			web3Loaded: false,
+			noWeb3: false
+		};
+	}
 
-    _pollWeb3() {
-	const component = this;
-	return new Promise(function(resolve, reject) {
-	    function poll() {
-		console.log("trying web3...");		
-		web3Api.setup() 
-		    .then(isWeb3Set => {
-	            
-		    if (!isWeb3Set) {
-			console.log("web3 is not set", isWeb3Set);
-			setTimeout(poll, 500);
-		    } else {
-			console.log("web3 is set", isWeb3Set);			
-			resolve();
-		    }
-		}).catch((err) => {
-		    console.log('Error finding web3.', err);
-		    reject(err);
-		});
-	    }
-	    
-	    poll();
-	});
-    }
-    
-    _getWeb3() {
-	const component = this;
-	return new Promise(function(resolve, reject) {
-	    component._pollWeb3()
-		.then(() => {
-		    resolve({ web3Loaded: true,  noWeb3: false});
-		}).catch(() => {
-		    resolve({ web3Loaded: true,  noWeb3: true});
-		});
-	});
-    }
-    
-    componentWillMount() {
-	this._getWeb3()
-	    .then(({ web3Loaded, noWeb3 }) => {
-		this.setState({ web3Loaded, noWeb3 });
-	    });
-  }
+	_pollWeb3() {
+		const component = this;
+		return new Promise(function (resolve, reject) {
+			function poll() {
+				console.log("trying web3...");
+				web3Api.setup()
+					.then(isWeb3Set => {
 
-    render(){
-	//console.log({state: this.state})	
-	return(
-		<div>
-		<Web3StatusBar web3Loaded={this.state.web3Loaded} noWeb3={this.state.noWeb3}/> 
-		<LinkFooter/>
-	   <div className="wrapper">
-		<div className="container-center animated slideInDown">
-		  <MainTab/>
-            </div>
-		</div>
-		
-		</div>
-	);
-  }
+						if (!isWeb3Set) {
+							console.log("web3 is not set", isWeb3Set);
+							setTimeout(poll, 500);
+						} else {
+							console.log("web3 is set", isWeb3Set);
+							resolve();
+						}
+					}).catch((err) => {
+						console.log('Error finding web3.', err);
+						reject(err);
+					});
+			}
+
+			poll();
+		});
+	}
+
+	_getWeb3() {
+		const component = this;
+		return new Promise(function (resolve, reject) {
+			component._pollWeb3()
+				.then(() => {
+					resolve({ web3Loaded: true, noWeb3: false });
+				}).catch(() => {
+					resolve({ web3Loaded: true, noWeb3: true });
+				});
+		});
+	}
+
+	componentWillMount() {
+		this._getWeb3()
+			.then(({ web3Loaded, noWeb3 }) => {
+				this.setState({ web3Loaded, noWeb3 });
+			});
+	}
+
+	render() {
+		//console.log({state: this.state})	
+		return (
+			<div>
+				<Web3StatusBar web3Loaded={this.state.web3Loaded} noWeb3={this.state.noWeb3} />
+				<div className="row" style={{ textAlign: "center", marginBottom: "15px" }}>
+
+					<h3>Send ethereum to any person by phone number.<br />Simple. Secure. No wallet needed.</h3>
+				</div>
+				<div className="wrapper">
+					<div className="container-center animated slideInDown">
+						<MainTab />
+					</div>
+				</div>
+				<LinkFooter />
+			</div>
+		);
+	}
 }
 
 
--- src/components/common/LinkFooter.js
+++ src/components/common/LinkFooter.js
@@ -8,17 +8,11 @@ class LinkFooter extends React.Component {
         //console.log(this.props);
 
         return (
-            <div>
-                <div className="row" style={{ textAlign: "center", marginBottom: "15px" }}>
-                    
-                        <h3>Send ethereum to any person by phone number.<br />Simple. Secure. No wallet needed.</h3>
-                </div>
-                <div className="row">
+                <div className="row" style={{marginBottom: "40px"}}>
                     <div className="col-md-2 col-md-offset-3"><a className="a-grey" href="/about.html"><div className="fa fa-info-circle links-margin" aria-hidden="true"> About</div></a></div>
                     <div className="col-md-2"><a className="a-grey" href="https://github.com/Dobrokhvalov/eth2phone"><div className="fa fa-github links-margin" aria-hidden="true"> Git</div></a></div>
                     <div className="col-md-2"><a className="a-grey" href="/FAQ.html"><div className="fa fa-question-circle-o links-margin" aria-hidden="true"> FAQ</div></a></div>
                 </div>
-            </div>
         );
     }
 }
