418668e37e6d6a516d333181ed261bc388041a44
Mikhail <m.dobrokhvalov@gmail.com>
2017-09-15 03:33:18 +0300

    phone number fixed

--- src/components/ReceiveTab/VerifiedProxy/PhoneForm.jsx
+++ src/components/ReceiveTab/VerifiedProxy/PhoneForm.jsx
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import Phone, { formatPhoneNumber, parsePhoneNumber, isValidPhoneNumber } from 'react-phone-number-input';
 import rrui from 'react-phone-number-input/rrui.css';
 import rpni from 'react-phone-number-input/style.css';
-import { parse, format, asYouType } from 'libphonenumber-js';
+import { parse, format, asYouType, isValidNumber } from 'libphonenumber-js';
 import serverApi from "../../../utils/quid-server-api";
 import sha3 from 'solidity-sha3';
 const util = require("ethereumjs-util");
 
 export default class ReceivePhoneTab extends Component {
     constructor(props) {
+	let phoneParams = {};
         super(props);
+	if (this.props.phone) {
+	    phoneParams = this._checkPhone(this.props.phone);
+	}
+	
         this.state = {
             phone: this.props.phone,
-            phoneCode: '',
-            phoneIsValid: false,
+            phoneCode: phoneParams.phoneCode,
+            phoneIsValid: phoneParams.phoneIsValid,
             code: this.props.code,
             error: "",
             isFetching: false
@@ -41,59 +46,52 @@ export default class ReceivePhoneTab extends Component {
         });
     }
 
-    greenTick() {
-		return (
-			<div className="green-tick" >&#10003;</div>
-		)
-	}
-
-    goBack() {
-        this.props.goBack();
+    _checkPhone(phone)  {
+        const phoneIsValid = isValidNumber(phone);
+        const formatter = new asYouType();
+        formatter.input(phone);
+        const phoneParams =  { phoneCode: formatter.country_phone_code, phone, phoneIsValid };
+	console.log({phoneParams});
+	return phoneParams;
     }
 
+
     render() {
-        console.log("RECEIVE: ", this.state.code, this.state.phone)
         const component = this;
         return (
             <form>
-                <div>
-                    <label>
-                        Your phone number
+              <div>
+                <label>
+                  Your phone number
                 </label>
-                </div>
-                <div className="input-container">
-						<div style={{ width: "90%", float: "left" }}>
-                    <Phone
-                        value={component.state.phone} onChange={phone => {
-                            const phoneIsValid = isValidPhoneNumber(phone);
-                            const formatter = new asYouType();
-                            formatter.input(phone);
-                            console.log(formatter.country_phone_code);
-                            this.setState({ phoneCode: formatter.country_phone_code, phone, phoneIsValid });
-                        }
-                        } /></div><div style={{ width: "10%", float: "right" }}>{this.state.phoneIsValid ? this.greenTick() : ""}</div></div>
+              </div>
+              <div className="input-container">
+		<div style={{ width: "90%", float: "left" }}>
+		  <Phone value={component.state.phone} onChange={phone => component.setState(component._checkPhone(phone))} />
+		</div>
+		<div style={{ width: "10%", float: "right" }}>{this.state.phoneIsValid ?  <div className="green-tick" >&#10003;</div> : ""}</div></div>
 
-                
-                <br />
-                <div>
-                    <label>
-                        Verification code
-                        </label>
-                </div>
-                <div className="input-container">
-						<div style={{ width: "90%", float: "left" }}>
-                    <input className="form-control" type="text" value={component.state.code} onChange={(event) => this.setState({ code: event.target.value })} />
-                    </div><div style={{ width: "10%", float: "right" }}>{this.state.code.length === 8 ? this.greenTick() : ""}</div></div>
-                <br />
-                <div>
-                    {this.state.isFetching ? <div className="loader-spin"></div> : ""}
+              
+              <br />
+              <div>
+                <label>
+                  Verification code
+                </label>
+              </div>
+              <div className="input-container">
+		<div style={{ width: "90%", float: "left" }}>
+                  <input className="form-control" type="text" value={component.state.code} onChange={(event) => this.setState({ code: event.target.value })} />
+                </div><div style={{ width: "10%", float: "right" }}>{this.state.code.length === 8 ? <div className="green-tick" >&#10003;</div> : ""}</div></div>
+              <br />
+              <div>
+                {this.state.isFetching ? <div className="loader-spin"></div> : ""}
 
-                    <a className="btn btn-md btn-default" onClick={() => component.goBack()}> Go Back </a>
-                    <a className="btn btn-md btn-accent btn-margin-left" onClick={() => component.submit()}> Submit </a>
+                <a className="btn btn-md btn-default" onClick={() => this.props.goBack()}> Go Back </a>
+                <a className="btn btn-md btn-accent btn-margin-left" onClick={() => component.submit()}> Submit </a>
 
-                    <span style={{ color: "red" }} > {component.state.error}</span>
+                <span style={{ color: "red" }} > {component.state.error}</span>
 
-                </div>
+              </div>
             </form>
         );
     }
