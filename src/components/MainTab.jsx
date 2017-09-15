919f41f1eec798b50dd520e9251fca4e1d4abae6
sashamihaha <sashamihaha@gmail.com>
2017-09-15 20:47:34 +0300

    tabs left

--- src/components/MainTab.jsx
+++ src/components/MainTab.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SendTab from './SendTab/SendTab';
 
-import ReceiveTab from  './ReceiveTab/ReceiveTab';
+import ReceiveTab from './ReceiveTab/ReceiveTab';
 import { getQueryParams } from '../utils/helpers';
 
 export default class Tab extends Component {
@@ -9,33 +9,36 @@ export default class Tab extends Component {
     constructor(props) {
         super(props);
 
-	const queryParams = getQueryParams();
+        const queryParams = getQueryParams();
         this.state = {
-            activeTab: (queryParams.tab || "sendTab"), 
+            activeTab: (queryParams.tab || "sendTab"),
             code: (queryParams.code || ""),
             phone: (queryParams.phone || "")
         };
     }
-    
-    changeTab(tabName){
-        this.setState({activeTab: tabName});
+
+    changeTab(tabName) {
+        this.setState({ activeTab: tabName });
     }
 
     render() {
         return (
-            <div className="tabs-container">
-              <ul className="nav nav-tabs">
-                <li className={("sendTab" === this.state.activeTab) ? "active" : ""}><a href="#" onClick={()=>this.changeTab("sendTab")}>Send</a></li>
-                <li className={("receiveTab" === this.state.activeTab) ? "active" : ""}><a href="#" onClick={()=>this.changeTab("receiveTab")}>Receive</a></li>
-              </ul>
-              <div className="tab-content">
-                <div id="tab-1" className="tab-pane active">
-                  <div className="panel-body">
-                    
-                    {("sendTab" === this.state.activeTab) ? <SendTab/> : <ReceiveTab code={this.state.code} phone={this.state.phone}/>}
-                  </div>
+            <div className="col-lg-12">
+                <div className="tabs-container">
+                    <div className="tabs-left">
+                        <ul className="nav nav-tabs">
+                            <li className={("sendTab" === this.state.activeTab) ? "active" : ""}><a href="#" onClick={() => this.changeTab("sendTab")}>Send</a></li>
+                            <li className={("receiveTab" === this.state.activeTab) ? "active" : ""}><a href="#" onClick={() => this.changeTab("receiveTab")}>Receive</a></li>
+                        </ul>
+                        <div className="tab-content">
+                            <div id="tab-6" className="tab-pane active">
+                                <div className="panel-body">
+                                    {("sendTab" === this.state.activeTab) ? <SendTab /> : <ReceiveTab code={this.state.code} phone={this.state.phone} />}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
         );
     }
--- src/components/SendTab/SendTab.jsx
+++ src/components/SendTab/SendTab.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import VerifiedProxyTab from './VerifiedProxy/VerifiedProxyTab';
 import web3Api from "../../apis/web3-common-api";
 
-function WrongNetworkMessage(){
+function WrongNetworkMessage() {
 
-    return(
+    return (
         <div>At this stage of the project we only support Ropsten network. Please switch to Ropsten in your web3 network provider.</div>
     )
 }
@@ -17,27 +17,15 @@ export default class Tab extends Component {
         };
     }
 
-    changeTab(tabName){
-        this.setState({activeTab: tabName});
+    changeTab(tabName) {
+        this.setState({ activeTab: tabName });
     }
 
 
-    
     render() {
         return (
-            <div className="tabs-container">
-                <div className="tabs-left">
-                    <ul className="nav nav-tabs">
-                        <li className={("verifiedProxy" === this.state.activeTab) ? "active" : ""}><a data-toggle="tab" href="#tab-6" onClick={() => this.changeTab("verifiedProxy")}>Verified Proxy</a></li>
-                    </ul>
-                    <div className="tab-content">
-                        <div id="tab-6" className="tab-pane active">
-                            <div className="panel-body">
-                                {web3Api.getNetworkId() === "3" ? <VerifiedProxyTab /> : <WrongNetworkMessage/> }
-                            </div>
-                       </div>
-                    </div>
-                </div>
+            <div>
+                {web3Api.getNetworkId() === "3" ? <VerifiedProxyTab /> : <WrongNetworkMessage />}
             </div>
         )
     }
