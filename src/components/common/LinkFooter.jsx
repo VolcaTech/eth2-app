15cb6d3664c047e18bdde5f1af31401dabc24487
Mikhail <m.dobrokhvalov@gmail.com>
2017-09-18 14:41:28 +0300

    about added

--- src/components/common/LinkFooter.jsx
+++ src/components/common/LinkFooter.jsx
@@ -8,11 +8,25 @@ class LinkFooter extends React.Component {
         //console.log(this.props);
 
         return (
-                <div className="row" style={{marginBottom: "40px"}}>
+	    <div>
+                <div className="row m-t-lg">
                     <div className="col-md-2 col-md-offset-3"><a className="a-grey" href="/about.html"><div className="fa fa-info-circle links-margin" aria-hidden="true"> About</div></a></div>
                     <div className="col-md-2"><a className="a-grey" href="https://github.com/Dobrokhvalov/eth2phone"><div className="fa fa-github links-margin" aria-hidden="true"> Git</div></a></div>
                     <div className="col-md-2"><a className="a-grey" href="/FAQ.html"><div className="fa fa-question-circle-o links-margin" aria-hidden="true"> FAQ</div></a></div>
                 </div>
+                <div className="row">
+		  <div className="col-sm-8 col-sm-offset-2">
+		  <p className="m-t-lg">
+		    <small>
+		      The current application serves the proof-of-concept reasons only and is deployed on Ropsten test
+                      network. Please be aware that systems bugs are possible at that stage. We will appreciate if
+                      you inform us any of them to <a style={{color: "#aaa", textDecoration: "underline"}} href="mailto: eth2phone@gmail.com"> eth2phone@gmail.com </a>
+		    </small>
+		  </p>
+		  </div>
+                </div>
+		
+	    </div>
         );
     }
 }
