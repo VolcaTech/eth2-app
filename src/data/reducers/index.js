ba61818ea2530ae1e16ac4990f6700762edbbd65
Alexander Milhaylov <sashamihaha@gmail.com>
2018-04-26 22:34:39 +0300

    create reducers

---
+++ src/data/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux'
+import { UPDATE_ADDRESS } from './../actions'
+
+function address(state = null, action) {
+  switch (action.type) {
+    case UPDATE_ADDRESS:
+      return action.address
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  address
+})
