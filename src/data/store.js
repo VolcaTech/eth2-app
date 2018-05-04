aced428c58d8911009510e086d2b21322a1f0094
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-04 03:29:33 +0300

    subscription for pending transfer added on page load

--- src/data/store.js
+++ src/data/store.js
@@ -3,6 +3,8 @@ import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { setupWeb3 } from '../actions/web3';
+import { subscribePendingTransfers } from '../actions/transfer';
+
 
 
 import reducers from './reducers';
@@ -18,11 +20,14 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 const enhancers = compose(applyMiddleware(thunk));
 const store = createStore(persistedReducer, undefined, enhancers);
 
-persistStore(store, null, () => {
+persistStore(store, null, async () => {
     console.log("configuring store..");
-    store.dispatch(setupWeb3());
-    const state = store.getState();
-    console.log({state});
+    // setup web3 data
+    await store.dispatch(setupWeb3());
+
+    // find all pending transfers and update status when they will be mined
+    store.dispatch(subscribePendingTransfers());
+    
 });
 
 
