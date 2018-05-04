771ca9d95053b8120b14ef7cc34da9276d9ec022
Alexander Milhaylov <sashamihaha@gmail.com>
2018-05-04 17:23:21 +0300

    checkbox css

--- src/components/SendTab/SendTab.jsx
+++ src/components/SendTab/SendTab.jsx
@@ -20,89 +20,88 @@ class Tab extends Component {
     constructor(props) {
         super(props);
         this.state = {
-	    amount: 0,
-            errorMessage: ""
+            amount: 0,
+            errorMessage: "",
+            buttonDisabled: true,
+            buttonOpacity: 0.1
         };
     }
 
-    async _sendTransfer({phone, phoneCode}) {
-	try {
-	    const transfer = await this.props.sendTransfer({
-	    	amount: this.state.amount,
-	    	phone,
-	    	phoneCode
-	    });
-	    this.props.history.push(`/transfers/${transfer.id}`);
-	} catch(err) {
-	    console.log(err);
-	    this.setState({ errorMessage: err.message });
-	}
+    async _sendTransfer({ phone, phoneCode }) {
+        try {
+            const transfer = await this.props.sendTransfer({
+                amount: this.state.amount,
+                phone,
+                phoneCode
+            });
+            this.props.history.push(`/transfers/${transfer.id}`);
+        } catch (err) {
+            console.log(err);
+            this.setState({ errorMessage: err.message });
+        }
     }
 
     _onSubmit() {
-	// hack for issue with phonenumber lib - https://github.com/bl00mber/react-phone-input-2/issues/10	
-	const phoneCode = this.phoneNumber.state.selectedCountry.dialCode;	
-	let phone = this.phoneNumber.state.formattedNumber;
-	// remove formatting from phone number
-	phone = "+" + phone.replace(/\D+/g, "");
-	
-	// check that phone number is valid
-	// if (!isValidPhoneNumber(phone)) {
-	//     this.setState({ errorMessage: "Phone number is invalid" });
-	//     return null;
-	// };
+        // hack for issue with phonenumber lib - https://github.com/bl00mber/react-phone-input-2/issues/10	
+        const phoneCode = this.phoneNumber.state.selectedCountry.dialCode;
+        let phone = this.phoneNumber.state.formattedNumber;
+        // remove formatting from phone number
+        phone = "+" + phone.replace(/\D+/g, "");
 
-	// check amount
-	if (this.state.amount <= 0) {
-	    this.setState({ errorMessage: "Amount should be more than 0" });
-	    return null;
-	};
+        // check that phone number is valid
+        // if (!isValidPhoneNumber(phone)) {
+        //     this.setState({ errorMessage: "Phone number is invalid" });
+        //     return null;
+        // };
 
-	
-	// this.setState({showPendingTransfer: true, step: 1});	
-	this._sendTransfer({phone, phoneCode});
+        // check amount
+        if (this.state.amount <= 0) {
+            this.setState({ errorMessage: "Amount should be more than 0" });
+            return null;
+        };
+
+
+        // this.setState({showPendingTransfer: true, step: 1});	
+        this._sendTransfer({ phone, phoneCode });
     };
 
     _renderForm() {
-	return (
-	    <div>
-	      <div style={{ marginBottom: 17 }}>
-		<NumberInput
-		   onChange={({target}) => {
-		       const amount = target.value;
-		       this.setState({amount});
-		  }}
-		  disabled={false}
-		  fontColor='black'
-		  backgroundColor='#fff'
-		  placeholder="amount (ETH)"
-		  />
-	     
-	      </div>
+        return (
+            <div>
+                <div>
+                    <NumberInput
+                        onChange={({ target }) => {
+                            const amount = target.value;
+                            this.setState({ amount });
+                        }}
+                        disabled={false}
+                        fontColor='black'
+                        backgroundColor='#fff'
+                        placeholder="amount (ETH)"
+                    />
 
-	      <div style={{ height: 28, color: '#ef4234', fontSize: 9, textAlign: 'center', paddingTop: 8 }}>
-		{this.state.errorMessage}
-	      </div>
-	      
-              <div style={{display: 'block', margin: 'auto', width: 295,}}>		
-		<PhoneInput _ref={(ref) => { this.phoneNumber = ref; }}/>
-	      </div>
-
-	      
-	      <div style={{ marginTop: 28 }}>
-		<ButtonPrimary handleClick={this._onSubmit.bind(this)} buttonColor={e2pColors.green}>
-		  Send
+                </div>
+                <div style={{ height: 28, color: '#ef4234', fontSize: 9, textAlign: 'center', paddingTop: 8 }}>
+                    {this.state.errorMessage}
+                </div>
+                <div style={{ display: 'block', margin: 'auto', width: 295, height: 39, marginBottom: 25 }}>
+                    <PhoneInput _ref={(ref) => { this.phoneNumber = ref; }} />
+                </div>
+                <div style={{marginBottom: 28}}>
+                    <ButtonPrimary handleClick={this._onSubmit.bind(this)} disabled={this.state.buttonDisabled} opacity={this.state.buttonOpacity} buttonColor={e2pColors.green}>
+                        Send
 		</ButtonPrimary>
-	      </div>
-	    </div>	    
-	);
+                </div>
+                <CheckBox onChange={() => this.setState({buttonDisabled: false, buttonOpacity: 1})}/>
+            </div>
+        );
     }
 
     render() {
         return (
             <div style={{ alignContent: 'center' }}>
-              <div><img src={e2pLogo} style={{ display: 'block', margin: 'auto', marginTop: 17, marginBottom: 28 }} /></div>	      
-	      { this._renderForm() }
+                <div><img src={e2pLogo} style={{ display: 'block', margin: 'auto', marginTop: 17, marginBottom: 28 }} /></div>
+                {this._renderForm()}
             </div>
         );
     }
--- src/components/common/CheckBox.jsx
+++ src/components/common/CheckBox.jsx
@@ -7,9 +7,9 @@ class CheckBox extends React.Component {
 
     render() {
         return (
-            <div style={{width: 223, height: 44, display: 'block', margin: 'auto'}}>
-            <div style={{verticalAlign: 'text-top', display: 'inline-block'}}><input type="radio" onChange={this.props.onChange}></input></div>
-            <div style={{ width: 205, height: 44, textAlign: 'center', display: 'inline-block', fontSize: 12, fontFamily: 'SF Display Regular'}}>I understand I am using alpha software, at my own risk, provided under MIT liscence.</div>
+            <div style={{width: 225, height: 44, display: 'block', margin: 'auto'}}>
+            <div style={{verticalAlign: 'text-top', display: 'inline-block', height: 15, width: 15, marginRight: 5}}><input type="radio" onChange={this.props.onChange}></input></div>
+            <div style={{ width: 205, height: 44, textAlign: 'center', display: 'inline-block', verticalAlign: 'text-top', fontSize: 12, fontFamily: 'SF Display Regular'}}>I understand I am using alpha software, at my own risk, provided under MIT liscence.</div>
     </div>
         );
     }
