d6aa57309363dcec8ed9b6296d66fb9bf41f1814
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-09 13:50:13 +0300

    web3 service code cleanup

--- src/services/web3Service.js
+++ src/services/web3Service.js
@@ -6,20 +6,6 @@ import { getTransactionReceiptMined, detectNetwork } from '../utils';
 
 const Web3Service = () => {
     let web3;
-    // 	_connected,
-    // 	_balance,
-    // 	_address,
-    // 	_networkId,
-    // 	_networkName;
-    
-    // let _loaded = false;
-    
-    function fetchBalance(addr) {
-	if (!addr) {
-	    return 0;
-	}
-	return web3.eth.getBalancePromise(addr);
-    }
     
     async function setup(){
 	// Get network provider and web3 instance.
@@ -32,7 +18,7 @@ const Web3Service = () => {
 	web3.eth.getTransactionReceiptMined = getTransactionReceiptMined;
 	
 	const address = web3.eth.accounts[0];
-	const balance = await fetchBalance(address);
+	const balance = await web3.eth.getBalancePromise(address);
 	const connected = web3.isConnected();
 	const { networkName, networkId } = detectNetwork(web3);
 
@@ -48,17 +34,7 @@ const Web3Service = () => {
 
     // api
     return {
-	//isConnected: () => {return _connected && web3.eth.accounts.length > 0;},
-	// getBalance: () => _balance,
-	//getAddress: () => _address,
-	//isLoaded: () => _loaded,
-	//getNetworkId: () => _networkId,
-	//getNetworkName: () => _networkName,
-	//toWei: (n, k) => web3.toWei(n, k),
-	//fromWei: (n, k) => web3.fromWei(n,k),
-	//toBigNumber: (n) => web3.toBigNumber(n),
 	getWeb3: () => web3,
-	//getTransactionReceiptMined: (txHash) => web3.eth.getTransactionReceiptMined(txHash, 500),
 	setup
     };
 }
