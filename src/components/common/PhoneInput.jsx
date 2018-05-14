218e14132f77131e1e9c197111985eb26a113d1a
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-14 21:14:54 +0300

    phone input style fixes

--- src/components/common/PhoneInput.jsx
+++ src/components/common/PhoneInput.jsx
@@ -1,20 +1,60 @@
 import React, { Component } from "react";
 import Phone from 'react-phone-input-2';
 
+const styles = {
+    input: {	
+	width: '100%',
+	height: 38,
+	borderRadius: 12,
+	color: 'black',
+	fontSize: 16,
+	fontFamily: "SF Display Bold",
+	letterSpacing: 1.94,
+	WebkitBoxShadow: 'none',
+	padding: 0,
+	textAlign: 'center',
+	border: '2px solid #f5f5f5',
+
+
+
+    },
+    flag: {
+	borderBottom: '2px solid #f5f5f5',
+	borderTop: '2px solid #f5f5f5',
+	borderLeft: '2px solid #f5f5f5',
+	borderRight: '0px',
+	borderRadius: 12,
+	borderBottomRightRadius: 0,
+	borderTopRightRadius: 0,
+	backgroundColor: 'white'
+    },    
+}
 
 class e2pPhoneInput extends React.Component {
-    
+
+    constructor(props) {
+	super(props);
+	this.state = {
+	    _phone: null
+	};
+    }
     
     render() {
+	console.log({phoneState: this.state});			 	
         return (
             <div style={{display: 'block', margin: 'auto', width: '78%',}}>
               <Phone
 		 ref={this.props._ref}		 
-         inputStyle={{ width: '100%', height: 38, borderRadius: 12, border: '2px solid #f5f5f5', color: 'black', fontSize: 16, fontFamily: "SF Display Bold", letterSpacing: 1.94, WebkitBoxShadow: 'none'}}
-                buttonStyle={{ borderBottom: '2px solid #f5f5f5', borderTop: '2px solid #f5f5f5', borderLeft: '2px solid #f5f5f5', borderRight: '0px', borderRadius: 12, borderBottomRightRadius: 0, borderTopRightRadius: 0, backgroundColor: 'white' }}
-                defaultCountry={'us'}
-              placeholder="Phone number"
-                
+		 inputStyle={styles.input}
+		 onFocus={() => {
+		     if (!this.state._phone) {
+
+			 this.setState({_phone: this.state._phone || "+1"});
+		     }
+		}}
+		buttonStyle={styles.flag}
+		placeholder="Phone number"
+                value={this.state._phone}
                    />
 </div>
         );
