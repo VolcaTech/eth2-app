6bc8b9b9cfd598bbbdae6547530d8d530d6d2fac
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-21 20:40:56 +0300

    Landing Form refactorign

--- src/landing/Form.jsx
+++ src/landing/Form.jsx
@@ -7,6 +7,102 @@ import ButtonPrimary from './../components/common/ButtonPrimary';
 import GotStuck from './../components/common/GotStuck';
 
 
+const styles = {
+    container: {
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between'
+    },
+    row: {
+	textAlign: 'center'
+    },
+    title: {
+	fontSize: 36,
+	fontFamily: 'SF Display Black',
+	textAlign: 'left',
+	lineHeight: 1,
+	marginBottom: 50
+    },
+    text: {
+	fontSize: 24,
+	fontFamily: 'SF Display Regular',
+	lineHeight: 1,
+	textAlign: 'left',
+	marginBottom: 50
+    },
+    faqLink: {
+	float: 'left',
+	color: '#0099ff',
+	fontFamily: 'SF Display Bold',
+	fontSize: 24,
+	textAlign: 'left',
+	paddingTop: 6,
+	marginBottom: 10
+    },
+    greyText: {
+	color: '#999999',
+	opacity: 0.8,
+	fontSize: 20,
+	fontFamily: 'SF Display Regular'
+    },
+    gitText: {
+	fontSize: 24,
+	fontFamily: 'SF Display Bold',
+	lineHeight: 1,
+	display: 'inline'
+    },    
+    column: {
+	display: 'block',
+	float: 'none'
+    },
+    gitContainer: {
+	width: '100%',
+	height: 46,
+	backgroundColor: '#fff',
+	margin: 'auto',
+	borderRadius: 10,
+	paddingBottom: 2,
+	paddingTop: 7,
+	border: '2px solid #000000',
+	marginBottom: 50
+    },
+    logo: { marginLeft: 10 },
+    aboutContainer: {
+	width: '80%',
+	margin: 'auto'
+    },
+    githubLink: {
+	textDecoration: 'none',
+	color: "#000" 
+    },
+    gitLogo: {
+	paddingBottom: 5,
+	marginRight: 5
+    },
+    right: {
+	float: 'right'
+    },
+    formContainer: {
+	width: '80%',
+	margin: 'auto',
+	marginBottom: 50,
+	marginTop: 50,
+	height: 350,
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between',
+	padding: 50,
+	borderRadius: 10,
+	boxShadow: '0px 0px 30px rgba(0, 0, 0, 0.1)'
+    },
+    sendLogo: {
+	width: '100%'
+    },
+    sendButton: {
+	textDecoration: 'none'
+    }
+}
+
 
 
 class Form extends React.Component {
@@ -17,32 +113,40 @@ class Form extends React.Component {
             <div>
                 <Row style={styles.row}>
                     <Col xs={12} md={6}  >
-                    <div style={{width: '80%', margin: 'auto'}}>
+                    <div style={styles.aboutContainer}>
                         <div style={styles.title}>Send Ether to everyone<br />
                             just by phone number</div>
                         <div style={styles.text}>Eth2Phone allows you to send Ether to
                         a person, verifying him by phone number.
 He could then receive the assets using link and any Ethereum address of his choice.</div>
                         <Row>
-                            <Col xs={12} md={6} style={styles.text2}>
-                            How it works?
-                            </Col>
-                            <Col xs={12} md={6} style={{float: 'right'}}>
-                            <Link to="/github" style={{ textDecoration: 'none', color: "#000" }}><div style={styles.gitContainer}>
-                                <img style={{paddingBottom: 5, marginRight: 5}} src={gitLogo}></img>
-                                <div style={styles.gitText}>View source</div>
-                            </div></Link>
+                          <Col xs={12} md={6}>
+			    <Link style={styles.faqLink} to="/faq">
+                              How it works?
+			    </Link>
+                          </Col>
+                            <Col xs={12} md={6} style={styles.right}>
+                              <a href="https://github.com/eth2phone/eth2phone-dapp" target="_blank" style={styles.githubLink}>
+				<div style={styles.gitContainer}>
+                                  <img style={styles.gitLogo} src={gitLogo}></img>
+                                  <div style={styles.gitText}>View source</div>
+				</div>
+			      </a>
                             </Col>
                         </Row>
-                        <div style={{marginBottom: 50}}><GotStuck/></div>
+                        <div style={{marginBottom: 50}}>
+			  <GotStuck/>
+			</div>
                 </div>
                     </Col>
                     <Col xs={12} md={5}>
-                        <div style={{width: '80%', margin: 'auto', marginBottom: 50, marginTop: 50, height: 350, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: 50, borderRadius: 10, boxShadow: '0px 0px 30px rgba(0, 0, 0, 0.1)'}}>
+                        <div style={styles.formContainer}>
                             <div style={{ fontSize: 24, fontFamily: 'SF Display Black' }}>Send Ether</div>
-                            <div><img style={{width: '100%'}} src={sendLogo}></img></div>
-                            <Link to="/">                            
-                            <ButtonPrimary buttonColor="#0099ff">Send</ButtonPrimary>
+                            <div>
+			      <img style={styles.sendLogo} src={sendLogo}></img>
+			    </div>
+                            <Link to="/" style={styles.sendButton}>                            
+                              <ButtonPrimary buttonColor="#0099ff">Send</ButtonPrimary>
                             </Link>
                             <div style={styles.greyText}>You should have metamask installed</div>
                         </div>
@@ -53,17 +157,5 @@ He could then receive the assets using link and any Ethereum address of his choi
     }
 }
 
-const styles = {
-    container: { display: 'flex', flexDirection: 'column', justifyContent: 'space-between' },
-    row: { textAlign: 'center' },
-    title: { fontSize: 36, fontFamily: 'SF Display Black', textAlign: 'left', lineHeight: 1, marginBottom: 50 },
-    text: { fontSize: 24, fontFamily: 'SF Display Regular', lineHeight: 1, textAlign: 'left', marginBottom: 50 },
-    text2: {float: 'left', color: '#0099ff', fontFamily: 'SF Display Bold', fontSize: 24, textAlign: 'left', paddingTop: 6, marginBottom: 10},
-    greyText: {color: '#999999', opacity: 0.8, fontSize: 20, fontFamily: 'SF Display Regular'},
-    gitText: { fontSize: 24, fontFamily: 'SF Display Bold', lineHeight: 1, display: 'inline' },    
-    column: { display: 'block', float: 'none' },
-    gitContainer: { width: '100%', height: 46, backgroundColor: '#fff', margin: 'auto', borderRadius: 10, paddingBottom: 2, paddingTop: 7, border: '2px solid #000000', marginBottom: 50 },
-    logo: { marginLeft: 10 }
-}
 
 export default Form;
