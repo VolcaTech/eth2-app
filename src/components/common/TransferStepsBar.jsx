6ca044d2c07f781bf57d826d8abc042a06674338
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-22 00:33:19 +0300

    transfer bar refactored

--- src/components/common/TransferStepsBar.jsx
+++ src/components/common/TransferStepsBar.jsx
@@ -1,45 +1,109 @@
 import React, { Component } from "react";
 import { ProgressBar } from 'react-bootstrap';
 
+const styles = {
+    container: {
+	width: 290,
+	display: 'block',
+	margin: 'auto' 
+    },
+    subcontainer: {
+	height: 20,
+	width: 250,
+	position: "relative",
+	marginBottom: 12,
+	marginLeft: 20,
+	marginRight: 20,
+	marginTop: 10 
+    },
+    progressBarContainer: {
+	width: 250,
+	height: 4,
+	position: "absolute",
+	marginTop: 8,
+	marginBottom: 8
+    },
+    progBar: {
+	width: 0,
+	height: 4,
+	backgroundColor: "#33aeff"
+    },    
+    dot1: {
+	height: 20,
+	width: 20,
+	backgroundColor: "#33aeff",
+	borderRadius: 40,
+	position: "absolute"
+    },    
+    dot2: {
+	height: 20,
+	width: 20,
+	backgroundColor: "#f5f5f5",
+	borderRadius: 40,
+	position: "absolute",
+	right: 115
+    },
+    dot3: {
+	height: 20,
+	width: 20,
+	backgroundColor: "#f5f5f5",
+	borderRadius: 40,
+	position: "absolute",
+	right: 0
+    },
+    redLineStyle: {
+	width: 120,
+	height: 4,
+	backgroundColor: "#f04234",
+	display: "flex"
+    },
+    createdLabel: {
+	width: 60,
+	height: 15,
+	textAlign: "center",
+	fontSize: 12
+    },
+    label: {
+	width: 60,
+	height: 15,
+	textAlign: "center",
+	fontSize: 12,
+	marginLeft: 55 
+    }
+}
+
 
 class e2pTransferBar extends React.Component {
 
     _renderTransferBar = (step) => {
         let title, progBarStyle, redLineStyle, dot1Style, dot2Style, dot3Style, textStyle1, textStyle2, textStyle3, pendingText;
         const labelStyle = { width: 60, height: 15, textAlign: "center", fontSize: 12 };
+	progBarStyle = {...styles.progBar};
+        dot1Style = {...styles.dot1};
+        dot2Style = {...styles.dot2};
+        dot3Style = {...styles.dot3};		
 
         switch (step) {
-            case 1:
-                progBarStyle = { width: 0, height: 4, backgroundColor: "#33aeff" };
-                dot1Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute" };
-                dot2Style = { height: 20, width: 20, backgroundColor: "#f5f5f5", borderRadius: 40, position: "absolute", right: 115 };
-                dot3Style = { height: 20, width: 20, backgroundColor: "#f5f5f5", borderRadius: 40, position: "absolute", right: 0 };
-                break;
-            case 2:
-                progBarStyle = { width: 120, height: 4, backgroundColor: "#33aeff" };
-                dot1Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute" };
-                dot2Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute", right: 115 };
-                dot3Style = { height: 20, width: 20, backgroundColor: "#f5f5f5", borderRadius: 40, position: "absolute", right: 0 };
-                break;
-            case 3:
-                progBarStyle = { width: 250, height: 4, backgroundColor: "#33aeff" };
-                dot1Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute" };
-                dot2Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute", right: 115 };
-                dot3Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute", right: 0 };
-                break;
-            case "fail":
-                progBarStyle = { width: 120, height: 4, backgroundColor: "#33aeff" };
-                redLineStyle = {width: 120 , height: 4, backgroundColor: "#f04234", display: "flex"};
-                dot1Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute" };
-                dot2Style = { height: 20, width: 20, backgroundColor: "#33aeff", borderRadius: 40, position: "absolute", right: 115 };
-                dot3Style = { height: 20, width: 20, backgroundColor: "#f04234", borderRadius: 40, position: "absolute", right: 0 };
-                break;
+        case 2:
+            progBarStyle.width = 120;
+	    dot2Style.backgroundColor = "#33aeff";
+            break;
+        case 3:
+            progBarStyle.width = 250;
+	    dot2Style.backgroundColor = "#33aeff";
+	    dot3Style.backgroundColor = "#33aeff";	    
+            break;
+        case "fail":
+            progBarStyle.width = 120;	    
+	    dot2Style.backgroundColor = "#33aeff";
+            break;
         }
         return (
-            <div style={{ width: 290, display: 'block', margin: 'auto' }}>
-                <div style={{ height: 20, width: 250, position: "relative", marginBottom: 12, marginLeft: 20, marginRight: 20, marginTop: 10 }}>
-                    <div className="progress" style={{ width: 250, height: 4, position: "absolute", marginTop: 8, marginBottom: 8, }}>
-                        <div className="progress-bar" role="progressbar" style={progBarStyle} ></div>{step === 'fail' ? <div style={redLineStyle}></div> : <div></div>}
+            <div style={styles.container}>
+                <div style={styles.subcontainer}>
+                    <div className="progress" style={styles.progressBarContainer}>
+                      <div className="progress-bar" role="progressbar" style={progBarStyle} ></div>
+		      {step === 'fail' ? <div style={styles.redLine}></div> : null}
                     </div>
                     <div className="dot" style={dot1Style}></div>
                     <div className={this.props.step === 2 ? 'dot scale-up-center' : 'dot'} style={dot2Style}></div>
@@ -47,9 +111,9 @@ class e2pTransferBar extends React.Component {
 
                 </div>
                 <div>
-                    <label style={{ width: 60, height: 15, textAlign: "center", fontSize: 12 }}>Created</label>
-                    <label style={{ width: 60, height: 15, textAlign: "center", fontSize: 12, marginLeft: 55 }}>Processing</label>
-                    <label style={{ width: 60, height: 15, textAlign: "center", fontSize: 12, marginLeft: 55 }}>{step === 'fail' ? 'Error' : 'Completed'}</label>
+                    <label style={styles.createdLabel}>Created</label>
+                    <label style={styles.label}>Processing</label>
+                    <label style={styles.label}>{step === 'fail' ? 'Error' : 'Completed'}</label>
                 </div>
             </div>
 
