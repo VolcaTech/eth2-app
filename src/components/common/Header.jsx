a878ee7b2bb22677f52301e527fda69be05a953f
Alexander Milhaylov <sashamihaha@gmail.com>
2018-05-23 00:43:31 +0300

    header fix

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -19,17 +19,18 @@ class e2pHeader extends React.Component {
     }
 
     render() {
-
+        console.log(window.innerWidth)
         const balance = this.props.balance < 1 ? this.props.balance.toFixed(4) : this.props.balance.toFixed(2);
         return (
-	      <Grid>
+            <Grid>
                 <Row>
                     <Col xs={12} sm={8} smOffset={2}>
 
                         <Row style={{ height: 44, display: 'block', margin: 'auto', backgroundColor: 'white', alignItems: 'center', borderTop: '2px solid #f5f5f5', marginBottom: 10 }}>
-                          <Col xs={6} style={{padding:0}}>
-                            <Link style={{ textDecoration: 'none' }} to="/" onClick={() => {
-				  if (window.location.hash && window.location.hash.length < 3) {window.location.reload();}}}>
+                            <Col xs={6} style={{ padding: 0 }}>
+                                <Link style={{ textDecoration: 'none' }} to="/" onClick={() => {
+                                    if (window.location.hash && window.location.hash.length < 3) { window.location.reload(); }
+                                }}>
                                     <div style={{ width: 55, height: 29, fontFamily: "SF Display Black", color: "black", fontSize: 24, letterSpacing: 1, textAlign: 'center', marginTop: 9 }}>
                                         Eth2<div style={{ letterSpacing: 0, display: 'inline', color: '#2bc64f' }}>Phone</div></div>
                                 </Link>
@@ -50,13 +51,14 @@ class e2pHeader extends React.Component {
                                         address={this.props.address} />
                                 </div>
 
-                                <div style={{ right: 0, marginTop: 16, float: 'right', fontFamily: "SF Display Bold" }}>
-                                    <div style={{ float: "left", fontSize: 12 }}>
+                                <div style={{ right: 0, marginTop: 16, float: 'right', fontFamily: "SF Display Bold", paddingTop: 1 }}>
+                                    <div style={{ float: "left", fontSize: 15 }}>
                                         {balance}
                                     </div>
-                                    <div style={{ float: "left", fontSize: 12, fontFamily: "SF Display Regular", fontWeight: 400, color: '#a9a9a9', marginLeft: 2 }}>
+                                    {window.innerWidth > '321' ?
+                                        (<div style={{ float: "left", fontSize: 15, fontFamily: "SF Display Regular", fontWeight: 400, color: '#a9a9a9' }}>
                                         ETH
-			  </div>
+     </div>) : <div></div>}
                                 </div>
                             </Col>
                         </Row>
@@ -69,7 +71,7 @@ class e2pHeader extends React.Component {
                             balance={this.props.balance} />
                     </Col>
                 </Row>
-	    </Grid>
+            </Grid>
 
         );
     }
@@ -78,7 +80,7 @@ class e2pHeader extends React.Component {
 const HeaderDetails = ({ height, address, contract, networkName, networkId, balance }) => {
     return (
         <div style={{ height: height, width: "100%", overflow: 'hidden', display: 'flex', justifyContent: 'space-between', boxShadow: '0px 30px 30px -35px rgba(0, 0, 0, 0.1)', alignItems: 'left', flexDirection: 'column', margin: "auto", textAlign: "left", marginBottom: 15 }}>
-            <div style={{marginTop: 8}}>
+            <div style={{ marginTop: 8 }}>
                 <div style={styles.headerDetailsGrey}>
                     ADDRESS
 	      </div>
@@ -94,24 +96,24 @@ const HeaderDetails = ({ height, address, contract, networkName, networkId, bala
                     {contract}
                 </div>
             </div>
-            <div style={{display: 'flex', flexDirection: 'row', width: '72%', justifyContent: 'space-between', marginBottom: 17}}>
-            <div>
-                <div style={styles.headerDetailsGrey}>
-                    NETWORK
+            <div style={{ display: 'flex', flexDirection: 'row', width: '72%', justifyContent: 'space-between', marginBottom: 17 }}>
+                <div>
+                    <div style={styles.headerDetailsGrey}>
+                        NETWORK
               </div>
-                <div style={styles.headerDetailsBlack}>
-                    {networkName}
-                    <i style={{ color: (networkId == '1' ? '#2bc64f' : 'orange'), fontSize: 8, marginLeft: 5, paddingTop: 4 }} className="fa fa-circle"></i>
+                    <div style={styles.headerDetailsBlack}>
+                        {networkName}
+                        <i style={{ color: (networkId == '1' ? '#2bc64f' : 'orange'), fontSize: 8, marginLeft: 5, paddingTop: 4 }} className="fa fa-circle"></i>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div style={styles.headerDetailsGrey}>
-                    BALANCE
+                <div>
+                    <div style={styles.headerDetailsGrey}>
+                        BALANCE
               </div>
-                <div style={styles.headerDetailsBlack}>
-                {balance} <div style={styles.headerDetailsGrey}>&nbsp;ETH</div>
+                    <div style={styles.headerDetailsBlack}>
+                        {balance} <div style={styles.headerDetailsGrey}>&nbsp;ETH</div>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
