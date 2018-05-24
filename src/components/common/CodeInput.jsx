0a568231adb46bff1d9ad53d9e095aa22c7b7b8e
Alexander Milhaylov <sashamihaha@gmail.com>
2018-05-25 00:41:47 +0300

    receive code unput fix

--- src/components/common/CodeInput.jsx
+++ src/components/common/CodeInput.jsx
@@ -5,20 +5,23 @@ import { FormControl } from 'react-bootstrap';
 
 const styles = {
     icon: {
-	display: 'inline-block',
-	width: '15%',
-	borderBottom: '2px solid #f5f5f5',
-	borderTop: '2px solid #f5f5f5',
-	borderRight: '2px solid #f5f5f5',
-	borderLeft: '0px',
-	borderRadius: 12,
-	height: 38,
-	borderBottomLeftRadius: 0,
-	borderTopLeftRadius: 0,
-	padding: 7.5,
-	backgroundColor: 'white',
-	textAlign: 'center'
-    },    
+        display: 'inline-flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '15%',
+        borderBottom: '2px solid #f5f5f5',
+        borderTop: '2px solid #f5f5f5',
+        borderRight: '2px solid #f5f5f5',
+        borderLeft: '0px',
+        borderRadius: 12,
+        height: 38,
+        borderBottomLeftRadius: 0,
+        borderTopLeftRadius: 0,
+        backgroundColor: 'white',
+        textAlign: 'center',
+        float: 'right'
+    },
 }
 
 
@@ -27,57 +30,59 @@ class e2pPhoneInput extends React.Component {
     render() {
         return (
             <div className="codeInput">
-              <FormControl
-		 onChange={this.props.onChange}
-		 disabled={this.props.disabled}
-		 componentClass='input'
-		 value={this.props.value}
-		 type={this.props.type || "number"}
-		 placeholder={this.props.placeholder}
-		 style={{
-                     width: '85%',
-                     height: 38,
-                     borderRadius: 12,
-		     border: '2px solid',
-                     borderColor: this.props.error ?  '#E64437' : '#f5f5f5',
-                     color: this.props.error ? '#E64437' : this.props.fontColor,		   
-                     backgroundColor: this.props.backgroundColor,
-                     fontSize: 16,
-                     letterSpacing: 1,
-                     textAlign: 'center',
-                     boxShadow: 0,
-                     display: 'inline-block', 
-                     fontFamily: "SF Display Bold",                 
-                     WebkitBoxShadow: 'none',
-		     borderRight: 0,
-		     borderTopRightRadius: 0,
-		     borderBottomRightRadius: 0,
-		     margin: 0,
-		     padding: 0
-		 }}>
-            </FormControl>
-	      <div
-		 style={{
-		     ...styles.icon,
-		     borderColor: this.props.error ?  '#E64437' : '#f5f5f5',
-		     }
-		   }>
-		<span
-		   className="hover"
-		   onClick={() => {
-		       alert("Please paste secret code from the sender's message.");
-		  }}
-		   style={{
-			  padding: 2,
-			  border: "1.5px solid #0099ff",
-			  borderRadius: 20,
-			  color: '#0099ff',
-			  paddingLeft: 6,
-			  paddingRight: 6
-		      }}>?</span>
-	      </div>		
+                <FormControl
+                    onChange={this.props.onChange}
+                    disabled={this.props.disabled}
+                    componentClass='input'
+                    value={this.props.value}
+                    type={this.props.type || "number"}
+                    placeholder={this.props.placeholder}
+                    style={{
+                        width: '85%',
+                        height: 38,
+                        borderRadius: 12,
+                        border: '2px solid',
+                        borderColor: this.props.error ? '#E64437' : '#f5f5f5',
+                        color: this.props.error ? '#E64437' : this.props.fontColor,
+                        backgroundColor: this.props.backgroundColor,
+                        fontSize: 16,
+                        letterSpacing: 1,
+                        textAlign: 'center',
+                        boxShadow: 0,
+                        display: 'inline-block',
+                        fontFamily: "SF Display Bold",
+                        WebkitBoxShadow: 'none',
+                        borderRight: 0,
+                        borderTopRightRadius: 0,
+                        borderBottomRightRadius: 0,
+                        margin: 0,
+                        padding: 0
+                    }}>
+                </FormControl>
+                <div
+                    style={{
+                        ...styles.icon,
+                        borderColor: this.props.error ? '#E64437' : '#f5f5f5',
+                    }
+                    }>
+                    <span
+                        className="hover"
+                        onClick={() => {
+                            alert("Please paste secret code from the sender's message.");
+                        }}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            border: "1px solid #0099ff",
+                            borderRadius: 20,
+                            color: '#0099ff',
+                            paddingLeft: 5,
+                            paddingRight: 6,
+                            paddingBottom: 1
+                        }}>?</span>
+                </div>
 
-	    </div>
+            </div>
         );
 
     }
