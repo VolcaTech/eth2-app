4a67c0de52d6b9c28c13d2b0d9ea25017c52bc6c
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-24 13:28:42 +0300

    Spinner style fixed

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
+const styles = {
+    loaderText: {
+	height: 40,
+	color: '#000000',
+	fontFamily: 'SF Display Bold',
+	fontSize: 12,
+	letterSpacing: 1.45,
+    },
+    error: {
+	color: '#E64437',
+	fontSize: 12,
+	fontFamily: 'SF Display Regular'
+    },
+    spinnerOrErrorContainer: {
+	height: 28,
+	textAlign: 'center',
+	paddingTop: 8
+    },
+    spinner: {
+	width: 20,
+	margin: 'auto'
+    }
+}
+
+
 const Spinner = () => (
     <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 );
@@ -8,23 +33,23 @@ const Spinner = () => (
 export const SpinnerOrError = ({fetching, error }) => {
     if (!(fetching || error)) { return null; }
     return (
-	    <div style={{ height: 28, textAlign: 'center', paddingTop: 8 }}>
+	    <div style={styles.spinnerOrErrorContainer}>
 	      { fetching ?
-		  <div style={{width: 20, margin: 'auto'}}>
+		  <div style={styles.spinner}>
 			<Spinner/>			
 		      </div> :
-		      <span style={{color: '#E64437', fontSize: 12,
-				    fontFamily: 'SF Display Regular'}}>{error}</span>
+		      <span style={styles.error}>{error}</span>
 		      }			   
 	    </div>
     );
 }
 
+
 export const Loader = ({text="Loading page...", textLeftMarginOffset=-15}) => (
     <div>
       <div className='centered-lds'>
 	<div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-	<div style={{color: '#0099FF', marginLeft: textLeftMarginOffset }}>{text}</div>	
+	<div style={{...styles.loaderText, marginLeft: textLeftMarginOffset }}>{text}</div>	
     </div>
     </div>
 
