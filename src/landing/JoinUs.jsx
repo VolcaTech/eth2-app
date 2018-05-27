56946a1ea3b3187166636c9fe81e5b8f3382d0d3
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-28 02:58:11 +0300

    reash us block styles

--- src/landing/JoinUs.jsx
+++ src/landing/JoinUs.jsx
@@ -5,11 +5,8 @@ import RetinaImage from 'react-retina-image';
 
 
 const styles = {
-    row: { textAlign: 'center', margin: 40 },
-    text: { color: '#0099ff', fontSize: 23, fontFamily: 'SF Display Bold', display: 'inline', lineHeight: 1, float: 'left', marginTop: 15, },
-    textWhite: { color: '#fff', fontSize: 23, fontFamily: 'SF Display Bold', display: 'inline', lineHeight: 1, marginTop: 3, marginLeft: 5 },
-    column: { display: 'inline-block', float: 'none', margin: 10 },
-    gitColumn: { backgroundColor: '#0099ff', margin: 'auto', borderRadius: 10, paddingBottom: 2, paddingTop: 7 },
+    text: { color: '#0099ff', marginTop: 15, },
+    textWhite: { color: '#fff', marginTop: 3, marginLeft: 5 },
     //logo: { marginLeft: 10 }
 }
 
@@ -20,46 +17,32 @@ class JoinUs extends React.Component {
     render() {
         return (
             <div className="reach-us-block">
-                <Row style={styles.row}>
-                    <div className="title">Reach us</div>
-                </Row>
+              <div className="title">Reach us</div>
 
-                <Row style={styles.row}>
+                <Row  className="reach-us-row">
                     <div style={{ margin: 'auto' }}>
-                        <Col xs={12} md={4} style={styles.column}>
-                            <a href="https://t.me/eth2io" target="_blank" style={{ textDecoration: 'none', color: "#000" }}>
-                                <Col xs={12} md={2}>
-                                    <RetinaImage style={styles.logo} src="https://eth2.io/images/telegram.png" />
-                                </Col>
-                                <Col xs={12} md={10}>
-                                    <div style={styles.text}>Chat with us on Telegram</div>
-                                </Col>
+                        <Col xs={12} sm={6}>
+                            <a href="https://t.me/eth2io" target="_blank">
+                              <RetinaImage style={styles.logo} src="https://eth2.io/images/telegram.png" />
+                              <span style={styles.text}>Chat with us on Telegram</span>
                             </a>
                         </Col>
-                        <Col xs={12} md={4} style={styles.column}>
-                            <a href="https://twitter.com/eth2io"  target="_blank" style={{ textDecoration: 'none', color: "#000" }}>
-                                <Col xs={12} md={2}>
-                                    <RetinaImage style={styles.logo} src="https://eth2.io/images/twitter.png" />
-                                </Col>
-                                <Col xs={12} md={10}>
-                                    <div style={styles.text}>Check our news on Twitter</div>
-                                </Col>
+                        <Col xs={12} sm={6} style={{textAlign: 'left'}} className="second-col">
+                          <a href="https://twitter.com/eth2io"  target="_blank" >
+                              <RetinaImage style={styles.logo} src="https://eth2.io/images/twitter.png" />
+                              <span style={styles.text}>Check our news on Twitter</span>
                             </a>
                         </Col>
-                    </div>
-                </Row>
 
-                <Row style={styles.row}>
-                    <div style={{ display: 'flex', alignContent: 'center' }}>
-                        <Col style={styles.gitColumn} xs={9} md={4}>
-                            <Link to="/github" style={{ textDecoration: 'none', color: "#000" }}>
-                                <div >
-                                    <RetinaImage style={{ marginBottom: 10, marginRight: 5 }} src="https://eth2.io/images/git.png" />
-                                    <div style={styles.textWhite}>Feel free to PR on Github</div>
-                                </div>
-                            </Link>
-                        </Col>
-                    </div>
+                        <Col xs={12} sm={4} smOffset={4}  className="third-col">	  
+                          <a href="https://github.com/eth2phone">
+                            <div className="gitButton" >
+                              <RetinaImage style={{ marginBottom: 10, marginRight: 5 }} src="https://eth2.io/images/git.png" />
+                              <span style={styles.textWhite}>Feel free to PR on Github</span>
+                            </div>
+                          </a>
+                        </Col>			
+                    </div>		    
                 </Row>
             </div>
         )
