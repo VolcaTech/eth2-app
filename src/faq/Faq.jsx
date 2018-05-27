8e968f55c71d8bb806908ad24197363d9ed8634f
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-05-27 20:51:43 +0300

    faq style for mobile fix

--- src/faq/Faq.jsx
+++ src/faq/Faq.jsx
@@ -5,119 +5,88 @@ import faqLogo from './../../public/images/faqillustration.png';
 import GotStuck from './../components/common/GotStuck';
 import scrollToElement from "scroll-to-element";
 const qs = require('querystring');
+import questions from './questions';
 
 
 const styles = {
-    container: { width: '75%', margin: 'auto' },
-    row: {
-        textAlign: 'center'
+    questionLinkContainer: {
+	textAlign: 'left',
+	marginBottom: 15
     },
-    col1: { padding: 0, marginBottom: 140 },
-    title: {
-        fontSize: 36,
-        fontFamily: 'SF Display Black',
-        textAlign: 'left',
-        lineHeight: 1,
-        marginBottom: 50
-    },
-    text: {
-        fontSize: 24,
-        fontFamily: 'SF Display Regular',
-        lineHeight: 1,
-        textAlign: 'left',
-        marginBottom: 50
-    },
-    questionLink: { textDecoration: 'none', fontSize: 24, textAlign: 'left', fontFamily: 'SF Display Bold', color: '#0099ff', textAlign: 'left', marginBottom: 15 },
-    questionLinkContainer: { marginBottom: 50 },
-    questionContainer: { width: '77%', margin: 'auto', textAlign: 'left', marginBottom: 75 },
-    question: { fontSize: 36, fontFamily: 'SF Display Black', marginBottom: 15 },
-    answer: { fontSize: 24, fontFamily: 'SF Display Regular' },
+    questionTitle: { marginBottom: 15 }
 }
 
-const questions = [
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    },
-    {
-        question: "Is this service fully trustless?",
-        answer: "Decentralisation vs Convenience trade-off. We acknowledge that our solution is not fully decentralized. You have to trust centralised entity to perform verification (SMS-verification), but ether transfers are trustless."
-    }
-]
 
 
 
 class Faq extends React.Component {
     componentDidMount() {
-	this.jumpToHash();
+	this._jumpToHash();
     }
     
-    jumpToHash() {
+    _jumpToHash() {
 	const queryParams = qs.parse(this.props.location.search.substring(1));
-	console.log({queryParams})
 	if (queryParams.q) {
 	    scrollToElement(`#q-${queryParams.q}`);
 	}
     }
 
+    _renderQuestionsMenu () {
+	return questions.map((question, index) => (
+            <Link to={{
+		      pathname: '/faq',
+		      search: `?q=${index}`
+		  }}
+		  className="no-underline link text"
+		  key={index}
+		  onClick={() => scrollToElement(`#q-${index}`)}
+	      >
+              <div style={styles.questionLinkContainer}>
+		{question.question}
+	      </div>
+            </Link>
+        ));
+    }
+
+    _renderQuestions() {
+	return questions.map((question, index) => (
+	    <div id={`q-${index}`} className="questionContainer" key={index}>
+	      <div style={styles.questionTitle} className="title">{question.question}</div>
+	      <div className="text">{question.answer}</div>
+	    </div>
+	));
+    }
+    
     render() {
         return (
-            <div>
-                <Row style={styles.row}>
-                    <div style={styles.container}>
-                        <div style={styles.title}>FAQ</div>
-                        <Col xs={12} md={6} style={styles.col1}>
-                          <div style={styles.questionLinkContainer}>{questions.map((question, index) => (
-                              <Link to={{
-					pathname: '/faq',
-					search: `?q=${index}`
-				    }}
-				    className="no-underline"  key={index} onClick={() => scrollToElement(`#q-${index}`)} >
-                                <div style={styles.questionLink}>
-				  {question.question}
-				</div>
-                                </Link>
-                            ))}</div>
-                            <GotStuck />
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <img src={faqLogo}></img>
-                        </Col>
-                    </div>
-                </Row>
-            {questions.map((question, index) => (
-                <div id={`q-${index}`} style={styles.questionContainer} key={index}>
-                  <div style={styles.question}>{question.question}</div>
-                  <div style={styles.answer}>{question.answer}</div>
-                </div>
-            )
-			  )}
+            <div className="faq-container">
+              <Row className="faq-header">
+                <Col xs={12} md={6}>
+		  <div>
+                    <div className="title">FAQ</div>
+		    
+                    <div className="questionsMenu">
+		      {this._renderQuestionsMenu()}
+	            </div>
+                    <GotStuck />
+		  </div>
+		</Col>
+		
+                <Col xs={12} md={6}>
+		  <div className="faq-logo hidden-xs">
+                    <img src={faqLogo}></img>
+		    </div>
+		</Col>
+		
+
+              </Row>
+	      <Row>
+		<Col xs={12}>
+		  {this._renderQuestions()}
+	    </Col>
+	    </Row>
             </div>
-        )
+        );
     }
 }
 
--- src/faq/index.jsx
+++ src/faq/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-
 import Faq from './Faq';
+import { Grid, Row, Col } from 'react-bootstrap';
+import './faq.css';
+
+
 
 
 
@@ -8,13 +11,20 @@ import Faq from './Faq';
 class e2pFaq extends React.Component {
     render() {
         return (
-            <div style={{width: '100%'}}>
-              <div style={{display: 'block', width: '100%', }}>
-		<Faq {...this.props}/>
-	      </div>                                 
-            </div>
+	    <div>
+              <Grid className="landing">
+		<Row>
+                  <Col xs={12}>
+		    <Faq/>
+		  </Col>		
+		</Row>
+	      </Grid>
+	      
+	    </div>
         )
     }
 }
+
+// <Footer/>
 
 export default e2pFaq;
